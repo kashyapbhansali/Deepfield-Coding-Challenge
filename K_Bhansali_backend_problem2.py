dd677d8b75ebd78f77e4e53f04da6c703393af68
Kashyap Bhansali <kashyapbhansali7@gmail.com>
2016-12-02 02:57:20 -0700

    backend coding complete with results.json

--- K_Bhansali_backend_problem2.py
+++ K_Bhansali_backend_problem2.py
@@ -30,7 +30,7 @@
 import urllib2
 # Get beautifulsoup4 with: pip install beautifulsoup4
 import bs4
-
+import json
 
 
 result = {}
@@ -52,33 +52,42 @@ def data_from_report(urls):
 		generate_output(link,country)
 
 def generate_output(url,country):
-	baseurl = "http://bgp.he.net/" + url
+	global result
+	baseurl = "http://bgp.he.net" + url
+	print baseurl
 	req = urllib2.Request(baseurl, headers={ 'User-Agent': 'Mozilla/5.0' })
 	html = urllib2.urlopen(req).read()
 	soup = bs4.BeautifulSoup(html,"html.parser")
 
 	rows = soup.find_all("tr")
-
+	
 	for row in rows:
 		datalist = row.find_all("td")
-		count = 1
+		count = 0
+		asn = 0
 		for d in datalist:
-			if count == 1:
-				asn_num = d.find("a").string
-				print asn_num
+			if count == 0:
+				asn_num = d.find("a").string[2:]
+				#json doesnt allow integer keys
+				asn = str(asn_num)
+				result[asn] = {}
+				result[asn]["Country"] = str(country)
 				count += 1
-			
-
-
-
-
-
+			else:
+				if count == 1:
+					temp = d.string
+					if temp:
+						result[asn]["Name"] = temp.encode('utf-8')
+				elif count == 3:
+					result[asn]["Routes v4"] = int(d.string.replace(',',''))
+				elif count == 5:
+					result[asn]["Routes v6"] = int(d.string.replace(',',''))
+				count += 1
 	
-
-
-			
-
 
 
 
 urls_from_main("http://bgp.he.net/report/world")
+
+with open("Results.json","w") as f:
+    json.dump(result,f)
